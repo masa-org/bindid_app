dd09f6acdef1293b488635909727ac2e772b8340
Ubuntu <ubuntu@ip-10-0-1-36.ap-northeast-1.compute.internal>
2022-01-13 09:20:02 +0000

    template

--- bindid.py
+++ bindid.py
@@ -1,10 +1,57 @@
-from flask import Flask
+from flask import Flask, request, render_template, abort
+
+from datetime import datetime
+import configparser
+import json
+import logging
+import logging.config
+
+log_level = {
+    'CRITICAL': 50,
+    'ERROR': 40,
+    'WARN': 30,
+    'INFO': 20,
+    'DEBUG': 10
+}
+
+# Global setting
+g_client_id = ""
+g_redirect_uri = ""
+        
+logger = logging.getLogger('app')
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+def read_config():
+  conf = configparser.ConfigParser()
+  with open('config.ini') as f:
+    conf.read_file(f)
+  return conf
 
 @app.route("/")
-def hello_world():
+def index():
+    return render_template('index.html', client_id=g_client_id, redirect_uri=g_redirect_uri)
+
+@app.route("/callback")
+def handle_():
     return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    logger.warn('In main...')
+    conf = read_config()
+
+    logging.basicConfig(
+        level = log_level[conf['DEFAULT']['LogLevel']],
+        format = '%(asctime)s - %(levelname)8s - %(name)9s - %(funcName)15s - %(message)s'
+    )
+    
+    try: 
+        g_client_id = conf['BINDID']['CLIENT_ID']
+        g_redirect_uri = conf['BINDID']['REDIRECT_URI']
+
+        app.run(host='0.0.0.0', port=int(conf['APP']['PORT'] ))
+
+    except Exception as e:
+        logging.error("There was an error starting the server: {}".format(e))
+
